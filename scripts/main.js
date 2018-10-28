cf3b9fa2f05cc404ccb9ae97c5e186217db92a81
furuhama <furuhama@users.noreply.github.com>
2018-10-28 12:03:50 +0900

    Add onclick event to make a main image changeable

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,14 @@
 
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello, world!';
+
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute('src', 'images/Chrome-icon.png');
+  } else {
+    myImage.setAttribute('src', 'images/firefox-icon.png');
+  }
+}
