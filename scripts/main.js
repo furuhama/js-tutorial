8172db0baf5cf2d772136b010c0e73e37adce23d
furuhama <furuhama@users.noreply.github.com>
2018-10-28 11:56:28 +0900

    Remove an onclick event, which is pretty noisy

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,3 @@
 
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello, world!';
-
-var i = 0;
-
-document.querySelector('h1').onclick = function() {
-  i++;
-  document.querySelector('h1').textContent = 'Hello, with count' + i;
-}
