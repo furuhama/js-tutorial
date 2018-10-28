ab1f3d5aed612432d4f79ff2697d14330cfe951f
furuhama <furuhama@users.noreply.github.com>
2018-10-28 11:55:21 +0900

    Set onclick event countable

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello, world!';
 
+var i = 0;
+
 document.querySelector('h1').onclick = function() {
-  alert('Hi, there.');
+  i++;
+  document.querySelector('h1').textContent = 'Hello, with count' + i;
 }
