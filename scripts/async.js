91eb5e1fd6db9d89c518a22a27d238cf0d2b93ca
furuhama <furuhama@users.noreply.github.com>
2018-11-25 20:57:53 +0900

    Add comment

--- scripts/async.js
+++ scripts/async.js
@@ -9,6 +9,9 @@ asyncBlockTime = (timeout) => {
 }
 
 console.log("Start script");
+// setTimeout runs asynchronously
+//
+// In JavaScript, almost all async functions runs on a main thread(a.k.a. UI thread).
 setTimeout(() => {
   console.log("Inside setTimeout() and Start asyncBlockTime()");
   asyncBlockTime(1000);
